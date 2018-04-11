d97cb070410b4d86899571928691b9ffffbbe993
tonypntsrn <tony.pantserno@gmail.com>
2018-04-11 19:28:54 +0300

    added firebase to project

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+    { id: 12, name: 'Andriy' },
+    { id: 13, name: 'Anton' },
+    { id: 14, name: 'Danil' },
+    { id: 15, name: 'Evita' },
+    { id: 16, name: 'Pavlo' },
+    { id: 17, name: 'Stepan' },
+    { id: 18, name: 'Kim' },
+    { id: 19, name: 'Gepart' },
+    { id: 20, name: 'Jo' },
+    { id: 21, name: 'Ko'},
+    { id: 22, name: 'Ro'},
+    { id: 23, name: 'Lo'}
+];
